1d12a0b30eba1323c371fb30ba198c373e008186
Gian Cortez <52141626+gccortez@users.noreply.github.com>
2019-07-19 18:18:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-<head>
 	<meta name="viewport"
 	      content="width=device-width, initial-scale=1.0">
+<head>
 	<title>Gian's Website</title>
 	<link rel="stylesheet" href="stylegian.css">
 </head>
